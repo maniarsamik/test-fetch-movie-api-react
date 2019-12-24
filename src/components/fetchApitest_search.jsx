405df8e249eea67455eb273f0004d5ce193d7528
Samik Maniar <samik.maniar@gatewaygroup.com>
2019-12-24 17:01:37 +0530

    updated

--- src/components/fetchApitest_search.jsx
+++ src/components/fetchApitest_search.jsx
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import axios from "axios";
-
+import styled from 'styled-components';
 class FetchApiTestSearch extends Component {
     constructor(props) {
         super(props);
-        this.state = { data: [], searchMovie: '', movieName: '', movieYear: '' };
+        this.state = { data: [], searchMovie: '', movieName: '', movieYear: '', top: -100, singleMovie:{}, };
 
     }
     search = event => {
         event.preventDefault();
-        axios
-            .get(
-
-                `https://www.omdbapi.com/?apikey=fc52ae74&s=${
-                this.state.searchMovie
-                }&plot=full`
-            )
+        axios.get(`https://www.omdbapi.com/?apikey=fc52ae74&s=${
+            this.state.searchMovie
+            }&plot=full`
+        )
             .then(res => res.data)
             .then(res => {
                 if (!res.Search) {
@@ -29,7 +26,6 @@ class FetchApiTestSearch extends Component {
             });
     };
     handleChange = event => {
-        console.log(event.target.value)
         this.setState({
             searchMovie: event.target.value
         });
@@ -37,12 +33,23 @@ class FetchApiTestSearch extends Component {
     deleteMovie = (i) => {
         const list = this.state.data;
         list.splice(i, 1);
-        this.setState({ data: list });
+        this.setState({
+            data: list, top: 16,
+        }, () => {
+            setTimeout(() => {
+                this.setState({
+                    top: -100,
+                });
+            }, 3000);
+        });
+    }
+    openDeleteModal = (d) => {
+        console.log(d);
+        this.setState({singleMovie:d})
     }
     editMovie = (d, i) => {
         console.log("test");
     }
-
     render() {
         const ListData = this.state.data.map((d, i) =>
             <div className="col mb-4" key={i}>
@@ -52,7 +59,7 @@ class FetchApiTestSearch extends Component {
                         <h5 className="card-title">{d.Title}</h5>
                         <p className="card-text"><small className="text-muted">{d.Year}</small></p>
                         <button className="btn btn-outline-secondary" onClick={() => this.editMovie(d, i)}>Edit</button>
-                        <button className="btn btn-outline-secondary" onClick={() => this.deleteMovie(i)}>Delete</button>
+                        <button className="btn btn-outline-secondary" data-toggle="modal" data-target="#exampleModal" onClick={() => this.openDeleteModal(d)}>Delete</button>
                     </div>
                 </div>
             </div>
@@ -63,7 +70,7 @@ class FetchApiTestSearch extends Component {
                     <div className="row">
                         <div className="col-12">
                             <form className="input-group mb-3" onSubmit={this.search}>
-                                <input type="text" className="form-control" ref="itemName" placeholder="Search for movies name" aria-describedby="button-addon2" onChange={this.handleChange} />
+                                <input type="text" className="form-control" placeholder="Search for movies name" aria-describedby="button-addon2" onChange={this.handleChange} />
                                 <div className="input-group-append">
                                     <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
                                 </div>
@@ -76,8 +83,29 @@ class FetchApiTestSearch extends Component {
                         </div>
                     </div>
                 </div>
+                <DeleteNotification top={this.state.top}>Deleted {this.state.singleMovie.Title} Movie from List</DeleteNotification>
+                <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Delete</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                Are you sure want to delete "{this.state.singleMovie.Title}" Movie from list?
+                            </div>
+                            <div className="modal-footer" style={{ backgroundColor: 'red', color: 'white' }}>
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-danger" onClick={() => this.deleteMovie(this.state.singleMovie.imdbID)} data-dismiss="modal">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+const DeleteNotification = styled.div`background-color: #ff0000; color: white; padding: 16px; position: absolute; top: ${props => props.top}px; right: 16px; z-index: 999; transition: top 0.5s ease;`;
 export default FetchApiTestSearch;
